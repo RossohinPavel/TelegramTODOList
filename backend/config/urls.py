682d1fae4315bf2397ea827165705a0630f7ab84
dfx_17 <rossohinpavel@yandex.ru>
2024-06-09 22:51:51 +0300

    точно, этот коммит

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from kanbanapi import views
+
+
+router = routers.SimpleRouter()
+router.register('', views.TasksViewSet, basename='tasks')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
+    path('api/<str:phone>/', include(router.urls))
 ]
