ca94e85ea0d04cd5e356759716f2327e080fa827
dfx_17 <rossohinpavel@yandex.ru>
2024-06-26 07:47:31 +0300

    tasks model tests

--- backend/tasks/tests.py
+++ backend/tasks/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import Task
+from users.models import User
+from django.core.exceptions import ValidationError
 
 # Create your tests here.
+class ModelTestCase(TestCase):
+
+    def test_validation(self):
+        """Тест валидации модели"""
+        now = timezone.now()
+        user = User.objects.create()
+        t = Task(
+            user=user,
+            title='test',
+            description='test',
+            actual_on=now,
+            finish_by=now
+        )
+        with self.assertRaises(ValidationError):
+            t.save()
