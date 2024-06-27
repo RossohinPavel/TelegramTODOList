2540249029fa12489b764644e7461083b439de06
dfx_17 <rossohinpavel@yandex.ru>
2024-06-27 09:54:11 +0300

    расширение модели user

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -4,7 +4,7 @@ from users.models import User
 
 # Зарегистрируем таким нетривиальным способом поле phone в модель админки
 fields = list(UserAdmin.fieldsets[1][1]['fields'])
-fields.append('phone')
+fields.extend(('phone', 'telegram_id'))
 UserAdmin.fieldsets[1][1]['fields'] = fields
 
 admin.site.register(User, UserAdmin)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 # Create your models here.
 class User(AbstractUser):
-    phone = models.CharField(blank=True)
+    phone = models.CharField(blank=True, validators=(RegexValidator(r'7\d{10}'), ))
+    telegram_id = models.PositiveIntegerField(blank=True, null=True)
