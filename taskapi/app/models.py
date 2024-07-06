b9cd23530398cd484cd55dea14405eaa21641b9a
dfx_17 <rossohinpavel@yandex.ru>
2024-07-06 14:09:27 +0300

    законченные базы данных в орм стиле

--- taskapi/app/models.py
+++ taskapi/app/models.py
@@ -1,20 +1,13 @@
-from sqlalchemy import String, text, func, DateTime
+from sqlalchemy import String, text, func, DateTime, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, validates
 from typing import Annotated
 from datetime import datetime
-
-
-
-# from sqlalchemy import Column, Integer, DateTime, Text, ForeignKey, Boolean
-# from sqlalchemy.ext.hybrid import hybrid_property
-
 
 
 class Base(DeclarativeBase):
     pass
 
 
-INTPK = Annotated[int, mapped_column(primary_key=True, autoincrement=True)]
 CREATED_AT = Annotated[datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"))]
 TIME = Annotated[datetime, mapped_column(DateTime(timezone=True))]
 
@@ -23,31 +16,30 @@ class User(Base):
     """Пользователи сервиса"""
     __tablename__ = 'users'
 
-    id: Mapped[INTPK]
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     created_at: Mapped[CREATED_AT]
 
-    phone_number: Mapped[str] = mapped_column(String(11), index=True, nullable=False, unique=True)
+    phone_number: Mapped[str] = mapped_column(String(11), index=True, unique=True)
     telegram_id: Mapped[int | None] = mapped_column(index=True)
 
 
 class Task(Base):
     __tablename__ = 'tasks'
 
-    id: Mapped[INTPK]
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
     created_at: Mapped[CREATED_AT]
     updated_at: Mapped[datetime] = mapped_column(
         server_default=text("TIMEZONE('utc', now())"),
         onupdate=func.utc_timestamp()
     )
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    
     # Контент
-    title: Mapped[str] = mapped_column(String(50), nullable=False)
+    title: Mapped[str] = mapped_column(String(50))
     description: Mapped[str | None]
     executed: Mapped[bool] = mapped_column(default=False)
     actual_on: Mapped[TIME]
     finish_by: Mapped[TIME]
-
-    # user_id = Column(Integer, ForeignKey('users.id'))
-    # user = relationship('User', back_populates='tasks')
 
     @validates("actual_on")
     def validate_actual_on(self, key, value):
