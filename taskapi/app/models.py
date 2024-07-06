99bc4ce45188391948aa91f4ed66c7cf5bef9ad8
dfx_17 <rossohinpavel@yandex.ru>
2024-07-06 13:46:39 +0300

    обновление модели

--- taskapi/app/models.py
+++ taskapi/app/models.py
@@ -1,12 +1,11 @@
-from sqlalchemy import String, text, func
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, text, func, DateTime
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, validates
 from typing import Annotated
 from datetime import datetime
 
 
 
 # from sqlalchemy import Column, Integer, DateTime, Text, ForeignKey, Boolean
-# from sqlalchemy.orm import relationship, validates
 # from sqlalchemy.ext.hybrid import hybrid_property
 
 
@@ -17,6 +16,7 @@ class Base(DeclarativeBase):
 
 INTPK = Annotated[int, mapped_column(primary_key=True, autoincrement=True)]
 CREATED_AT = Annotated[datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"))]
+TIME = Annotated[datetime, mapped_column(DateTime(timezone=True))]
 
 
 class User(Base):
@@ -43,31 +43,28 @@ class Task(Base):
     title: Mapped[str] = mapped_column(String(50), nullable=False)
     description: Mapped[str | None]
     executed: Mapped[bool] = mapped_column(default=False)
-
-    # actual_on = Column(DateTime(timezone=True), nullable=False)
-    # finish_by = Column(DateTime(timezone=True), nullable=False)
-
-    # executed = Column(Boolean(), default=False)
+    actual_on: Mapped[TIME]
+    finish_by: Mapped[TIME]
 
     # user_id = Column(Integer, ForeignKey('users.id'))
     # user = relationship('User', back_populates='tasks')
 
-    # actual_on = Column(DateTime(timezone=True), nullable=False)
-    # finish_by = Column(DateTime(timezone=True), nullable=False)
+    @validates("actual_on")
+    def validate_actual_on(self, key, value):
+        self.validate_time(value, self.finish_by)
+        return value
 
-    # executed = Column(Boolean(), default=False)
-
-    # @validates("actual_on")
-    # def validate_actual_on(self, key, value):
-    #     if self.finish_by is not None and value >= self.finish_by:
-    #         raise ValueError("finish_by should be great then actual_on")
-    #     return value
-
-    # @validates("finish_by")
-    # def validate_finish_by(self, key, value):
-    #     if self.actual_on is not None and value <= self.actual_on:
-    #         raise ValueError("finish_by should be great then actual_on")
-    #     return value
+    @validates("finish_by")
+    def validate_finish_by(self, key, value):
+        self.validate_time(self.actual_on, value)
+        return value
+    
+    def validate_time(self, actual_on: datetime, finish_by: datetime) -> None:
+        """Функция для сравнения времени"""
+        if actual_on is None or finish_by is None:
+            return
+        if actual_on >= finish_by:
+            raise ValueError("finish_by should be great then actual_on")
     
     # @hybrid_property
     # def status(self):
