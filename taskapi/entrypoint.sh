df4c6aa4a84208ee58a99a5b737529b30181a2d5
dfx_17 <rossohinpavel@yandex.ru>
2024-07-13 08:25:54 +0300

    приборка проекта

--- taskapi/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo --Ожидание подключения к базе данных Postgress
-# ./wait-for
-echo --Подключено. Применяются миграции
-python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
-# exec "$@"
